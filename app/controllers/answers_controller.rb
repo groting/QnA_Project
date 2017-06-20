e5cd5ffaaca43bcfa42bfdeed0d2f7066a8dd704
groting <kyleop@mail.ru>
2017-06-20 15:43:36 +0400

    Fix errors messages

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,13 +7,7 @@ class AnswersController < ApplicationController
   def create
     @answer = @question.answers.new(answer_params)
     @answer.user = current_user
-
-    if @answer.save
-      #flash[:notice] = 'Your answer successfully created.'
-    else
-      flash[:notice] = 'Error prevent answer from saving.'
-      render 'questions/show'
-    end
+    @answer.save
   end
 
   def destroy
