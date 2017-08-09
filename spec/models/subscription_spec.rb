c158f1b5704cfab2e0240d3347a5f2d56a8f2e15
groting <groting24@gmail.com>
2017-08-09 20:19:36 +0300

    Validatesfix

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
-  it { should validate_presence_of :question_id}
-  it { should validate_presence_of :user_id}
+  it { should validate_uniqueness_of(:question_id).scoped_to(:user_id)}
 end
