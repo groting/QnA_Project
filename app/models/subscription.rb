abd0f937ee59ee60d1247301b13e74b7a9360fe6
groting <groting24@gmail.com>
2017-08-09 20:19:36 +0300

    Validatesfix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :question
-  validates :user_id, presence: true, uniqueness: { scope: :user_id }
-  validates :question_id, presence: true
+  
+  validates :question_id, uniqueness: { scope: :user_id }
 end
